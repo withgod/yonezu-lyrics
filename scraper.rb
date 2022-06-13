2ef06b23d1b681431d3ba993aa6b29f24e57a91a
withgod <noname@withgod.jp>
2022-06-13 14:05:35 +0900

    update

--- scraper.rb
+++ scraper.rb
@@ -5,14 +5,23 @@ require "rubygems"
 require "nokogiri"
 require "pp"
 
-base     = "http://j-lyric.net"
-start    = base + "/artist/a0579b7/"
+base     = "http://j-lyric.net/"
+artist_id = "a0579b7"
 sleep    = 1 #sec
 override = true
 
+if ARGV.length >= 1
+    artist_id = ARGV[0]
+end
+
+start = base + '/artist/' +  artist_id + '/'
+
+out_dir = './out/' + artist_id
+Dir.mkdir(out_dir , 0755)
+
+
 index_html = open(start)
 index_doc = Nokogiri::HTML.parse(index_html, nil, 'utf-8')
-
 
 p index_doc.title
 
@@ -22,6 +31,7 @@ titles.each do |title|
     id        = title["href"].split('/')[3].split('.')[0]
     lyric_url = base + title["href"]
 
+    p lyric_url
     lyric_html = open(lyric_url)
 
     lyric_doc = Nokogiri::HTML.parse(lyric_html, nil, 'utf-8')
@@ -33,7 +43,8 @@ titles.each do |title|
     end
 
     #p lyric.text
-    File.write('out/' + id + '.txt', title.text + "\n--\n" + lyric.text)
+
+    File.write(out_dir + '/' + id + '.txt', title.text + "\n--\n" + lyric.text)
 
     sleep(sleep)
 end
